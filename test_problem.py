bd0fbc720375fde84e90dd14d01ec49221c0c3bc
Lalit Yadav <lalit.yadav@datagrokr.com>
2022-01-17 16:20:53 +0530

    testing again

--- test_problem.py
+++ test_problem.py
@@ -2,7 +2,7 @@ import operator
 import json
 words = []
 ops = { "+": operator.add,
-        "-": operator.sub,
+        "-": operator.sub, 
         "*": operator.mul}
 def checknumber(a):
     try:
@@ -15,7 +15,7 @@ def checknumber(a):
 def checkoperator(b):
     operators = ['+','-','*']
     return b in operators
-
+    
 def solver(words):
     if (checknumber(words[0]) and checknumber(words[2]) and checkoperator(words[1])):
         print(ops[words[1]](int(words[0]),int(words[2])))
@@ -23,12 +23,12 @@ def solver(words):
     else:
         print("please check your syntax.e.g. 3+5")
         return None
+
 def lambda_handler(event,context):
+    print("working good")
     print(event)
-    print("successful")
     input_exp = event['exp']
     stripped = input_exp.replace(" ","")
     for i in stripped:
         words.append(i)
     return solver(words)
-
